f5d4460740359448b7fb00cc71ccaba1be503f18
Roman <romankos1993@gmail.com>
2022-12-28 19:52:45 +0300

    homework 004 init

---
+++ homework004/homework004.py
@@ -0,0 +1,55 @@
+# Задана натуральная степень k. Сформировать случайным образом список
+# коэффициентов (значения от 0 до 100) многочлена и записать в файл многочлен
+# степени k.
+# Пример:
+# - k=2 => 2*x² + 4*x + 5 = 0 или x² + 5 = 0 или 10*x² = 0
+"""Doc."""
+import random
+import os
+os.system('cls')
+
+
+def write_file(st):
+    with open('file.txt', 'w') as data:
+        data.write(st)
+
+
+def rnd():
+    return random.randint(0, 101)
+
+
+def create_mn(k):
+    lst = [rnd() for _ in range(k+1)]
+    return lst
+
+
+def create_str(sp):
+    lst = sp[::-1]
+    wr = ''
+    if len(lst) < 1:
+        wr = 'x = 0'
+    else:
+        for i in range(len(lst)):
+            if i != len(lst) - 1 and lst[i] != 0 and i != len(lst) - 2:
+                wr += f'{lst[i]}x^{len(lst)-i-1}'
+                if lst[i+1] != 0:
+                    wr += ' + '
+            elif i == len(lst) - 2 and lst[i] != 0:
+                wr += f'{lst[i]}x'
+                if lst[i+1] != 0:
+                    wr += ' + '
+            elif i == len(lst) - 1 and lst[i] != 0:
+                wr += f'{lst[i]} = 0'
+            elif i == len(lst) - 1 and lst[i] == 0:
+                wr += ' = 0'
+    return wr
+
+
+def main():
+    k = int(input("Enter natural degree k = "))
+    coefficient = create_mn(k)
+    write_file(create_str(coefficient))
+
+
+if __name__ == '__main__':
+    main()
